3166b0fbcba907f010c5f8d2fc25bf4b25546438
jeremyAparicio <jeremy.aparicio.g@gmail.com>
2022-11-03 01:37:22 +0000

    Se genero random card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbols = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let type = ["♦", "♥", "♠", "♣"];
+
+  let figure = Math.floor(Math.random() * type.length);
+  let value = Math.floor(Math.random() * symbols.length);
+
+  let valueSymbols = document.querySelector(".symbols");
+  valueSymbols.innerHTML = symbols[value];
+
+  let figureType = document.querySelectorAll(".type");
+  figureType[0].innerHTML = type[figure];
+  figureType[1].innerHTML = type[figure];
+
+  if (type[figure] == "♦") {
+    figureType[0].style.color = "red";
+    figureType[1].style.color = "red";
+  } else if (type[figure] == "♥") {
+    figureType[0].style.color = "red";
+    figureType[1].style.color = "red";
+  }
 };
